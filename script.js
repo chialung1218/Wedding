56419a600ec1d12d48176957255b7e288253f2f6
chialung1218 <chialung1218@gmail.com>
2026-06-28 22:48:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,49 @@ function updateCountdown() {
 updateCountdown();
 
 setInterval(updateCountdown,1000);
+
+document.getElementById("submitBtn")?.addEventListener("click", async () => {
+
+    const data = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        attend: document.getElementById("attend").value,
+        guests: document.getElementById("guests").value,
+        vegetarian: document.getElementById("vegetarian").value,
+        message: document.getElementById("message").value
+    };
+
+    if (!data.name) {
+        alert("請輸入姓名");
+        return;
+    }
+
+    const btn = document.getElementById("submitBtn");
+
+    btn.disabled = true;
+    btn.innerText = "送出中...";
+
+    try {
+
+        await fetch(
+            "https://script.google.com/macros/s/AKfycbzb2HdsccLRGPWIiGEosv9AepxopbLfG4vgmgJAx5E4NH67sbFaDIOmFmpT-PEZlKLIXw/exec",
+            {
+                method: "POST",
+                body: JSON.stringify(data)
+            }
+        );
+
+        alert("感謝您的回覆 ❤️");
+
+        document.getElementById("rsvpForm").reset();
+
+    } catch (error) {
+
+        alert("送出失敗，請稍後再試");
+
+    }
+
+    btn.disabled = false;
+    btn.innerText = "送出回覆";
+
+});
